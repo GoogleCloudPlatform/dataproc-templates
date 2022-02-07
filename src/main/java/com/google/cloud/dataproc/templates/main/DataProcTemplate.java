3c4f72711fabd7d9ebf4dbad2d5cba36f3bcc162
Shashank Agarwal <agarwalsh@google.com>
2022-02-06 21:43:08 -0800

    update pattern

--- src/main/java/com/google/cloud/dataproc/templates/main/DataProcTemplate.java
+++ src/main/java/com/google/cloud/dataproc/templates/main/DataProcTemplate.java
@@ -165,7 +165,7 @@ public class DataProcTemplate {
   private static void testTempLocationAccess(TemplateName templateName) {
     try{
       String USER_AGENT_HEADER = "user-agent";
-      String USER_AGENT_VALUE = "google-pso-tool/dataproc-templates/0.1.0/" + templateName;
+      String USER_AGENT_VALUE = "google-pso-tool/dataproc-templates/0.1.0-" + templateName;
 
       HeaderProvider headerProvider =
               FixedHeaderProvider.create(ImmutableMap.of(USER_AGENT_HEADER, USER_AGENT_VALUE));
