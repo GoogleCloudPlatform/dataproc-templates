613f00a9a8791d0bace0b0a95424847f0181b75b
Pablo Paglilla <ppaglilla@google.com>
2022-04-12 04:48:29 +0000

    [Python] Add pyspark dependency to setup.py file

--- python/setup.py
+++ python/setup.py
@@ -4,5 +4,8 @@ setuptools.setup(
     name="dataproc-templates",
     version="0.0.1",
     description="Dataproc templates written in Python",
-    packages=setuptools.find_packages(exclude=['test'])
+    packages=setuptools.find_packages(exclude=['test']),
+    install_requires=[
+        'pyspark>=3.2.0'
+    ]
 )
