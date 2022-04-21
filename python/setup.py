86affe1f586a629f07043516edcc97690341af7b
Pablo Paglilla <ppaglilla@google.com>
2022-04-21 05:44:12 +0000

    [Python] Add project URL to setup.py

--- python/setup.py
+++ python/setup.py
@@ -60,6 +60,7 @@ setup(
     name="dataproc-templates",
     version="0.0.1",
     description="Dataproc templates written in Python",
+    url="https://github.com/GoogleCloudPlatform/dataproc-templates",
     packages=find_packages(exclude=['test']),
     install_requires=[
         'pyspark>=3.2.0'
