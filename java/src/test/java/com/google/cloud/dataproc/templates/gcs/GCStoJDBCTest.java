549f0d6f4b8957fc22af54a09e7b3cda55177daf
HITESH HASIJA <hhasija@google.com>
2022-06-01 10:47:40 +0530

    handling exception error in unit testing file for GCS to JDBC

--- java/src/test/java/com/google/cloud/dataproc/templates/gcs/GCStoJDBCTest.java
+++ java/src/test/java/com/google/cloud/dataproc/templates/gcs/GCStoJDBCTest.java
@@ -15,70 +15,66 @@
  */
 package com.google.cloud.dataproc.templates.gcs;
 
-import static com.google.cloud.dataproc.templates.gcs.GCSToJDBCConfig.GCS_JDBC_INPUT_FORMAT;
-import static com.google.cloud.dataproc.templates.gcs.GCSToJDBCConfig.GCS_JDBC_INPUT_LOCATION;
-import static com.google.cloud.dataproc.templates.gcs.GCSToJDBCConfig.GCS_JDBC_OUTPUT_BATCH_INSERT_SIZE;
-import static com.google.cloud.dataproc.templates.gcs.GCSToJDBCConfig.GCS_JDBC_OUTPUT_DRIVER;
-import static com.google.cloud.dataproc.templates.gcs.GCSToJDBCConfig.GCS_JDBC_OUTPUT_SAVE_MODE;
-import static com.google.cloud.dataproc.templates.gcs.GCSToJDBCConfig.GCS_JDBC_OUTPUT_TABLE;
-import static com.google.cloud.dataproc.templates.gcs.GCSToJDBCConfig.GCS_JDBC_OUTPUT_URL;
-import static com.google.cloud.dataproc.templates.gcs.GCSToSpannerConfig.*;
+import static com.google.cloud.dataproc.templates.util.TemplateConstants.*;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static com.google.cloud.dataproc.templates.gcs.GCSToJDBCConfig.*;
+import org.junit.jupiter.api.function.ThrowingSupplier;
 
 import com.google.cloud.dataproc.templates.util.PropertyUtil;
-import com.google.cloud.dataproc.templates.util.ValidationUtil.ValidationException;
-import jakarta.validation.ConstraintViolation;
 import java.util.stream.Stream;
+import org.apache.spark.sql.SparkSession;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.function.ThrowingSupplier;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-class GCStoJDBCTest {
+public class GCStoJDBCTest {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(GCStoJDBCTest.class);
 
   @BeforeEach
   void setUp() {
+    SparkSession spark = SparkSession.builder().master("local").getOrCreate();
+  }
+
+  @ParameterizedTest
+  @MethodSource("propertyKeys")
+  void runTemplateWithValidParameters(String propKey) {
+    LOGGER.info("Running test: runTemplateWithValidParameters");
     PropertyUtil.getProperties().setProperty(GCS_JDBC_INPUT_FORMAT, "some value");
     PropertyUtil.getProperties().setProperty(GCS_JDBC_INPUT_LOCATION, "some value");
     PropertyUtil.getProperties().setProperty(GCS_JDBC_OUTPUT_DRIVER, "some value");
     PropertyUtil.getProperties().setProperty(GCS_JDBC_OUTPUT_TABLE, "some value");
     PropertyUtil.getProperties().setProperty(GCS_JDBC_OUTPUT_URL, "some value");
-    PropertyUtil.getProperties().setProperty(GCS_JDBC_OUTPUT_BATCH_INSERT_SIZE, "some value");
     PropertyUtil.getProperties().setProperty(GCS_JDBC_OUTPUT_SAVE_MODE, "some value");
-  }
 
-  @Test
-  void runTemplateWithValidParameters() {
-    LOGGER.info("Running test: runTemplateWithValidParameters");
     assertDoesNotThrow((ThrowingSupplier<GCSToJDBC>) GCSToJDBC::of);
   }
 
   @ParameterizedTest
-  @MethodSource("requiredPropertyKeys")
-  void runTemplateWithMissingRequiredParameters(String propKey) {
+  @MethodSource("propertyKeys")
+  void runTemplateWithInvalidParameters(String propKey) {
     LOGGER.info("Running test: runTemplateWithInvalidParameters");
     PropertyUtil.getProperties().setProperty(propKey, "");
-    ValidationException exception = assertThrows(ValidationException.class, GCSToJDBC::of);
-    assertEquals(1, exception.getViolations().size());
-    ConstraintViolation<?> violation = exception.getViolations().get(0);
-    assertEquals("must not be empty", violation.getMessage());
+
+    Exception exception =
+            assertThrows(IllegalArgumentException.class, GCSToJDBC::of);
+    assertEquals(
+            "Required parameters for GCStoJDBC not passed. "
+                    + "Set mandatory parameter for GCStoJDBC template",
+            exception.getMessage());
   }
 
-  static Stream<String> requiredPropertyKeys() {
+  static Stream<String> propertyKeys() {
     return Stream.of(
-        GCS_JDBC_INPUT_FORMAT,
-        GCS_JDBC_INPUT_LOCATION,
-        GCS_JDBC_OUTPUT_DRIVER,
-        GCS_JDBC_OUTPUT_TABLE,
-        GCS_JDBC_OUTPUT_URL,
-        GCS_JDBC_OUTPUT_BATCH_INSERT_SIZE,
-        GCS_JDBC_OUTPUT_SAVE_MODE);
+            GCS_JDBC_INPUT_FORMAT,
+            GCS_JDBC_INPUT_LOCATION,
+            GCS_JDBC_OUTPUT_DRIVER,
+            GCS_JDBC_OUTPUT_TABLE,
+            GCS_JDBC_OUTPUT_URL,
+            GCS_JDBC_OUTPUT_SAVE_MODE);
   }
 }
