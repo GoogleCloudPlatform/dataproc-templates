93bf9052a1c648e0816b43bc31ef869b42116a58
Anish <anishks@google.com>
2022-07-05 14:59:39 +0530

    Spotless formating

--- java/src/main/java/com/google/cloud/dataproc/templates/main/DataProcTemplate.java
+++ java/src/main/java/com/google/cloud/dataproc/templates/main/DataProcTemplate.java
@@ -69,7 +69,7 @@ public class DataProcTemplate {
           .put(TemplateName.S3TOBIGQUERY, (args) -> new S3ToBigQuery())
           .put(TemplateName.SPANNERTOGCS, (args) -> new SpannerToGCS())
           .put(TemplateName.JDBCTOBIGQUERY, (args) -> new JDBCToBigQuery())
-              .put(TemplateName.HBASETOGCS, (args) -> new HbaseToGCS())
+          .put(TemplateName.HBASETOGCS, (args) -> new HbaseToGCS())
           .put(TemplateName.JDBCTOGCS, (args) -> new JDBCToGCS())
           .put(TemplateName.KAFKATOBQ, (args) -> new KafkaToBQ())
           .put(TemplateName.GCSTOJDBC, GCSToJDBC::of)
