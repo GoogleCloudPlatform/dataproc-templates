8050813e99067c8e9a1c43231ce6b6f4398275ce
Nilo Resende <nilofr@google.com>
2022-08-11 17:17:45 +0000

    fix header constant

--- python/dataproc_templates/hbase/hbase_to_gcs.py
+++ python/dataproc_templates/hbase/hbase_to_gcs.py
@@ -114,7 +114,7 @@ class HbaseToGCSTemplate(BaseTemplate):
                 .save(output_location)
         elif output_format == constants.FORMAT_CSV:
             writer \
-                .option(constants.CSV_HEADER, True) \
+                .option(constants.HEADER, True) \
                 .csv(output_location)
         elif output_format == constants.FORMAT_JSON:
             writer.json(output_location)
