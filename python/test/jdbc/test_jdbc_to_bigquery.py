196db5df2c7d13c0d3cdac60d843a2829d037f35
sjlva <rafaelsilva@posteo.net>
2022-09-05 15:51:15 +0100

    add mock unit tests

--- python/test/jdbc/test_jdbc_to_bigquery.py
+++ python/test/jdbc/test_jdbc_to_bigquery.py
@@ -14,12 +14,14 @@
  * limitations under the License.
 """
 
+import mock
+import pyspark
 
 from dataproc_templates.jdbc.jdbc_to_bigquery import JDBCToBigQueryTemplate
 import dataproc_templates.util.template_constants as constants
 
 
-class TestJDBCToGCSTemplate:
+class TestJDBCToBigQueryTemplate:
     """
     Test suite for JDBCToBigQueryTemplate
     """
@@ -54,7 +56,7 @@ class TestJDBCToGCSTemplate:
         assert parsed_args["jdbc.bigquery.output.table"] == "bq-table"  
         assert parsed_args["jdbc.bigquery.temp.bucket.name"] == "bucket-name"  
 
-    def test_run_pass_args2(self):
+    def test_run_pass_args3(self):
         """Tests JDBCToBigQueryTemplate pass args"""
 
         jdbc_to_bigquery_template = JDBCToBigQueryTemplate()
@@ -77,3 +79,77 @@ class TestJDBCToGCSTemplate:
         assert parsed_args["jdbc.bigquery.output.table"] == "bq-table"  
         assert parsed_args["jdbc.bigquery.temp.bucket.name"] == "bucket-name"  
         
+    @mock.patch.object(pyspark.sql, 'SparkSession')
+    def test_run_pass_args3(self, mock_spark_session):
+        """Tests JDBCToBigQueryTemplate pass args"""
+
+        jdbc_to_bigquery_template = JDBCToBigQueryTemplate()
+
+        mock_parsed_args = jdbc_to_bigquery_template.parse_args(
+            ["--jdbc.bigquery.input.url=url",
+             "--jdbc.bigquery.input.driver=driver",
+             "--jdbc.bigquery.input.table=table1",
+             "--jdbc.bigquery.input.partitioncolumn=column",
+             "--jdbc.bigquery.input.lowerbound=1",
+             "--jdbc.bigquery.input.upperbound=2",
+             "--jdbc.bigquery.numpartitions=5",
+             "--jdbc.bigquery.output.mode=append",
+             "--jdbc.bigquery.output.dataset=bq-dataset",
+             "--jdbc.bigquery.output.table=bq-table",
+             "--jdbc.bigquery.temp.bucket.name=bucket-name",
+             ])       
+        mock_spark_session.read.format().option().option().option().option().option().option().option().load.return_value = mock_spark_session.dataframe.DataFrame
+        jdbc_to_bigquery_template.run(mock_spark_session, mock_parsed_args)
+        mock_spark_session.read.format.assert_called_with(constants.FORMAT_JDBC)
+        mock_spark_session.read.format().option.assert_called_with(constants.JDBC_URL, "url")
+        mock_spark_session.read.format().option().option.assert_called_with(constants.JDBC_DRIVER, "driver")
+        mock_spark_session.read.format().option().option().option.assert_called_with(constants.JDBC_TABLE, "table1")
+        mock_spark_session.read.format().option().option().option().option.assert_called_with(constants.JDBC_PARTITIONCOLUMN, "column")
+        mock_spark_session.read.format().option().option().option().option().option.assert_called_with(constants.JDBC_LOWERBOUND, "1")
+        mock_spark_session.read.format().option().option().option().option().option().option.assert_called_with(constants.JDBC_UPPERBOUND, "2")
+        mock_spark_session.read.format().option().option().option().option().option().option().option.assert_called_with(constants.JDBC_NUMPARTITIONS, "5")
+        mock_spark_session.read.format().option().option().option().option().option().option().option().load()
+        mock_spark_session.dataframe.DataFrame.write.format.assert_called_once_with(
+            constants.FORMAT_BIGQUERY)
+        mock_spark_session.dataframe.DataFrame.write.format(
+        ).option.assert_called_once_with(constants.TABLE, "bq-dataset.bq-table")
+        mock_spark_session.dataframe.DataFrame.write.format().option(
+        ).option.assert_called_once_with(constants.JDBC_BQ_TEMP_BUCKET, "bucket-name")
+        mock_spark_session.dataframe.DataFrame.write.format(
+        ).option().option().mode.assert_called_once_with(constants.OUTPUT_MODE_APPEND)
+        mock_spark_session.dataframe.DataFrame.write.format(
+        ).option().option().mode().save.assert_called_once()
+
+    @mock.patch.object(pyspark.sql, 'SparkSession')
+    def test_run_pass_args4(self, mock_spark_session):
+        """Tests JDBCToBigQueryTemplate pass args"""
+
+        jdbc_to_bigquery_template = JDBCToBigQueryTemplate()
+
+        mock_parsed_args = jdbc_to_bigquery_template.parse_args(
+            ["--jdbc.bigquery.input.url=url",
+             "--jdbc.bigquery.input.driver=driver",
+             "--jdbc.bigquery.input.table=table1",
+             "--jdbc.bigquery.output.mode=append",
+             "--jdbc.bigquery.output.dataset=bq-dataset",
+             "--jdbc.bigquery.output.table=bq-table",
+             "--jdbc.bigquery.temp.bucket.name=bucket-name",
+             ])
+        mock_spark_session.read.format().option().option().option().option().load.return_value = mock_spark_session.dataframe.DataFrame
+        jdbc_to_bigquery_template.run(mock_spark_session, mock_parsed_args)
+        mock_spark_session.read.format.assert_called_with(constants.FORMAT_JDBC)
+        mock_spark_session.read.format().option.assert_called_with(constants.JDBC_URL, "url")
+        mock_spark_session.read.format().option().option.assert_called_with(constants.JDBC_DRIVER, "driver")
+        mock_spark_session.read.format().option().option().option.assert_called_with(constants.JDBC_TABLE, "table1")
+        mock_spark_session.read.format().option().option().option().option.assert_called_with(constants.JDBC_NUMPARTITIONS, "10")
+        mock_spark_session.read.format().option().option().option().option().load()
+        mock_spark_session.dataframe.DataFrame.write.format.assert_called_once_with(
+            constants.FORMAT_BIGQUERY)
+        mock_spark_session.dataframe.DataFrame.write.format(
+        ).option.assert_called_once_with(constants.TABLE, "bq-dataset.bq-table")
+        mock_spark_session.dataframe.DataFrame.write.format().option(
+        ).option.assert_called_once_with(constants.JDBC_BQ_TEMP_BUCKET, "bucket-name")
+        mock_spark_session.dataframe.DataFrame.write.format(
+        ).option().option().mode.assert_called_once_with(constants.OUTPUT_MODE_APPEND)
+        mock_spark_session.dataframe.DataFrame.write.format(
+        ).option().option().mode().save.assert_called_once()
