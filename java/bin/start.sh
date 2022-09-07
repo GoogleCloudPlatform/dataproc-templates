4f679373ac17d11a7fcb4d5b56b8fbbd9cd73791
Rafael Silva <63989100+sjlva@users.noreply.github.com>
2022-09-07 21:44:26 +0100

    Update start.sh

--- java/bin/start.sh
+++ java/bin/start.sh
@@ -165,3 +165,6 @@ ${command} "$@"
 spark_status=$?
 
 check_status $spark_status "\n Spark Command ran successful \n" "\n It seems like there are some issues in running spark command. Requesting you to please go through the error to identify issues in your code \n"
+
+echo "\n Feel free to reach out dataproc-templates-support-external@googlegroups.com if you have a question \n"
+echo "\n Also consider giving a feedback at: https://forms.gle/XXCJeWeCJJ9fNLQS6 \n"
