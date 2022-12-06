a9e4a348c873607af840891b937cd995d2661e2a
Sebastian Montes <sebass@google.com>
2022-12-06 17:42:55 -0500

    removed not needed import statement

--- python/main.py
+++ python/main.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pipes import Template
 from typing import Dict, Any, Type
 import logging
 import sys
