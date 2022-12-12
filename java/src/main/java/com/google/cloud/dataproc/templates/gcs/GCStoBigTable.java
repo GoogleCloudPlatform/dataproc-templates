b01727fb1b27f6bd10ef4e2d878e6315c2f796ed
balajiss <balajiss@google.com>
2022-12-12 21:30:23 +0530

    test

--- java/src/main/java/com/google/cloud/dataproc/templates/gcs/GCStoBigTable.java
+++ java/src/main/java/com/google/cloud/dataproc/templates/gcs/GCStoBigTable.java
@@ -17,40 +17,40 @@ package com.google.cloud.dataproc.templates.gcs;
 
 import static com.google.cloud.dataproc.templates.util.TemplateConstants.*;
 
+import com.google.cloud.bigtable.data.v2.BigtableDataClient;
+import com.google.cloud.bigtable.data.v2.models.RowMutation;
 import com.google.cloud.dataproc.templates.BaseTemplate;
+import java.util.Iterator;
 import java.util.Objects;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.spark.api.java.function.ForeachPartitionFunction;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class GCStoBigTable implements BaseTemplate {
+public class GCStoBigTable implements BaseTemplate, java.io.Serializable {
 
   public static final Logger LOGGER = LoggerFactory.getLogger(GCStoBigTable.class);
 
   private String projectID;
   private String inputFileLocation;
-  private String bigQueryDataset;
-  private String bigQueryTable;
+  private String bigTableInstanceId;
+  private String bigTableTableName;
+  private String bigTableProjectId;
+  private String bigTableColumnFamily;
   private String inputFileFormat;
-  private String bqTempBucket;
-
-  private String bqTempTable;
-
-  private String bqTempQuery;
 
   public GCStoBigTable() {
 
     projectID = getProperties().getProperty(PROJECT_ID_PROP);
-    inputFileLocation = getProperties().getProperty(GCS_BQ_INPUT_LOCATION);
-    bigQueryDataset = getProperties().getProperty(GCS_OUTPUT_DATASET_NAME);
-    bigQueryTable = getProperties().getProperty(GCS_OUTPUT_TABLE_NAME);
-    inputFileFormat = getProperties().getProperty(GCS_BQ_INPUT_FORMAT);
-    bqTempBucket = getProperties().getProperty(GCS_BQ_LD_TEMP_BUCKET_NAME);
-    bqTempTable = getProperties().getProperty(GCS_BQ_TEMP_TABLE);
-    bqTempQuery = getProperties().getProperty(GCS_BQ_TEMP_QUERY);
+    inputFileLocation = getProperties().getProperty(GCS_BT_INPUT_LOCATION);
+    bigTableInstanceId = getProperties().getProperty(GCS_BT_OUTPUT_INSTANCE_ID);
+    bigTableTableName = getProperties().getProperty(GCS_BT_OUTPUT_TABLE_NAME);
+    bigTableProjectId = getProperties().getProperty(GCS_BT_OUTPUT_PROJECT_ID);
+    bigTableColumnFamily = getProperties().getProperty(GCS_BT_OUTPUT_TABLE_COLUMN_FAMILY);
+    inputFileFormat = getProperties().getProperty(GCS_BT_INPUT_FORMAT);
   }
 
   @Override
@@ -61,11 +61,7 @@ public class GCStoBigTable implements BaseTemplate {
     LOGGER.info("input format: {}", inputFileFormat);
 
     try {
-      spark =
-          SparkSession.builder()
-              .appName("GCS to BigTable load")
-              .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
-              .getOrCreate();
+      spark = SparkSession.builder().appName("GCS to Bigtable load").getOrCreate();
 
       Dataset<Row> inputData = null;
 
@@ -90,54 +86,38 @@ public class GCStoBigTable implements BaseTemplate {
               "Currently avro, parquet and csv are the only supported formats");
       }
 
-      if (bqTempTable != null && bqTempQuery != null) {
-        inputData.createOrReplaceGlobalTempView(bqTempTable);
-        inputData = spark.sql(bqTempQuery);
-      }
-
-      /*inputData
-      .write()
-      .format(GCS_BQ_OUTPUT_FORMAT)
-      .option(GCS_BQ_CSV_HEADER, true)
-      .option(GCS_BQ_OUTPUT, new String())
-      .option(GCS_BQ_TEMP_BUCKET, bqTempBucket)
-      .mode(SaveMode.Append)
-      .save();*/
-
       inputData.foreachPartition(
-          new bigtableWrite()
-          /*       new ForeachPartitionFunction<Row>() {
-
+          new ForeachPartitionFunction<Row>() {
             public void call(Iterator<Row> t) throws Exception {
-              LOGGER.info("Inside Call");
-              long timestamp1 = System.currentTimeMillis() * 1000;
+
               BigtableDataClient dataClient =
-                  BigtableDataClient.create("yadavaja-sandbox", "bt-templates-test");
+                  BigtableDataClient.create(bigTableProjectId, bigTableInstanceId);
+
               while (t.hasNext()) {
 
+                long timestamp = System.currentTimeMillis() * 1000;
                 Row row = t.next();
-
-                LOGGER.info("The message is %s ", row.getString(2));
-                String rowkey = "aaaaa";
-                String columnfamily = "cf";
-                String columnname = "latitude";
-                String columnvalue = "100";
                 try {
                   RowMutation rowMutation =
-                      RowMutation.create("bus-data", rowkey)
-                          .setCell(columnfamily, columnname, timestamp1, columnvalue);
+                      RowMutation.create(bigTableTableName, row.get(0).toString());
 
+                  for (int i = 0; i < row.size(); i++) {
+                    rowMutation.setCell(
+                        bigTableColumnFamily,
+                        row.schema().fieldNames()[i],
+                        timestamp,
+                        row.get(i).toString());
+                  }
                   dataClient.mutateRow(rowMutation);
-                  LOGGER.info("Successfully wrote row %s", rowkey);
+
                 } catch (Exception e) {
                   LOGGER.info("Error during WriteSimple: \n" + e.toString());
                 }
               }
+
               dataClient.close();
             }
-
-          }*/
-          );
+          });
 
     } catch (Throwable th) {
       LOGGER.error("Exception in GCStoBigTable", th);
@@ -150,21 +130,23 @@ public class GCStoBigTable implements BaseTemplate {
   void validateInput() {
     if (StringUtils.isAllBlank(projectID)
         || StringUtils.isAllBlank(inputFileLocation)
-        || StringUtils.isAllBlank(bigQueryDataset)
-        || StringUtils.isAllBlank(bigQueryTable)
+        || StringUtils.isAllBlank(bigTableInstanceId)
+        || StringUtils.isAllBlank(bigTableTableName)
         || StringUtils.isAllBlank(inputFileFormat)
-        || StringUtils.isAllBlank(bqTempBucket)) {
+        || StringUtils.isAllBlank(bigTableColumnFamily)
+        || StringUtils.isAllBlank(bigTableProjectId)) {
       LOGGER.error(
           "{},{},{},{},{},{} are required parameter. ",
           PROJECT_ID_PROP,
-          GCS_BQ_INPUT_LOCATION,
-          GCS_OUTPUT_DATASET_NAME,
-          GCS_OUTPUT_TABLE_NAME,
-          GCS_BQ_INPUT_FORMAT,
-          GCS_BQ_LD_TEMP_BUCKET_NAME);
+          GCS_BT_INPUT_LOCATION,
+          GCS_BT_OUTPUT_INSTANCE_ID,
+          GCS_BT_OUTPUT_TABLE_NAME,
+          GCS_BT_INPUT_FORMAT,
+          GCS_BT_OUTPUT_TABLE_COLUMN_FAMILY,
+          GCS_BT_OUTPUT_PROJECT_ID);
       throw new IllegalArgumentException(
-          "Required parameters for GCStoBQ not passed. "
-              + "Set mandatory parameter for GCStoBQ template "
+          "Required parameters for GCStoBT not passed. "
+              + "Set mandatory parameter for GCStoBT template "
               + "in resources/conf/template.properties file.");
     }
 
@@ -175,15 +157,17 @@ public class GCStoBigTable implements BaseTemplate {
             + "3. {}:{}"
             + "4. {}:{}"
             + "5. {}:{}",
-        GCS_BQ_INPUT_LOCATION,
+        GCS_BT_INPUT_LOCATION,
         inputFileLocation,
-        GCS_OUTPUT_DATASET_NAME,
-        bigQueryDataset,
-        GCS_OUTPUT_TABLE_NAME,
-        bigQueryTable,
-        GCS_BQ_INPUT_FORMAT,
+        GCS_BT_OUTPUT_INSTANCE_ID,
+        bigTableInstanceId,
+        GCS_BT_OUTPUT_TABLE_NAME,
+        bigTableTableName,
+        GCS_BT_INPUT_FORMAT,
         inputFileFormat,
-        GCS_BQ_LD_TEMP_BUCKET_NAME,
-        bqTempBucket);
+        GCS_BT_OUTPUT_TABLE_COLUMN_FAMILY,
+        bigTableColumnFamily,
+        GCS_BT_OUTPUT_PROJECT_ID,
+        bigTableProjectId);
   }
 }
