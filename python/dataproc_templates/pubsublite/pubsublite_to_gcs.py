d7a628126fa6048e2679177d780b50b9e72e06f2
saumyasinha-google <saumyasinha@google.com>
2023-03-15 22:45:22 +0530

    fix: minor changes

--- python/dataproc_templates/pubsublite/pubsublite_to_gcs.py
+++ python/dataproc_templates/pubsublite/pubsublite_to_gcs.py
@@ -22,85 +22,63 @@ class PubsubliteToGCSTemplate(BaseTemplate):
         parser: argparse.ArgumentParser = argparse.ArgumentParser()
 
         parser.add_argument(
-            f'--{constants.PUBSUBLITE_TO_GCS_INPUT_PROJECT_ID}',
-            dest=constants.PUBSUBLITE_TO_GCS_INPUT_PROJECT_ID,
-            required=True,
-            help='Pubsublite Project ID'
-        )
-        parser.add_argument(
             f'--{constants.PUBSUBLITE_TO_GCS_OUTPUT_PROJECT_ID}',
             dest=constants.PUBSUBLITE_TO_GCS_OUTPUT_PROJECT_ID,
             required=True,
             help='GCS Project ID'
         )
-        # parser.add_argument(
-        #     f'--{constants.PUBSUBLITE_TO_GCS_INPUT_TOPIC}',
-        #     dest=constants.PUBSUBLITE_TO_GCS_INPUT_TOPIC,
-        #     required=False,
-        #     help='Pubsublite to GCS Input topic name'
-        # )
         parser.add_argument(
             f'--{constants.PUBSUBLITE_TO_GCS_INPUT_SUBSCRIPTION}',
             dest=constants.PUBSUBLITE_TO_GCS_INPUT_SUBSCRIPTION,
             required=True,
             help='Pubsublite to GCS Input subscription name'
         )
-        # parser.add_argument(
-        #     f'--{constants.PUBSUBLITE_TO_GCS_INPUT_TIMEOUT_MS}',
-        #     dest=constants.PUBSUBLITE_TO_GCS_INPUT_TIMEOUT_MS,
-        #     required=False,
-        #     default=60000,
-        #     help='Stream timeout, for how long the subscription will be read'
-        # )
-        # parser.add_argument(
-        #     f'--{constants.PUBSUBLITE_TO_GCS_STREAMING_DURATION_SECONDS}',
-        #     dest=constants.PUBSUBLITE_TO_GCS_STREAMING_DURATION_SECONDS,
-        #     required=False,
-        #     default=15,
-        #     help='Streaming duration, how often wil writes to GCS be triggered'
-        # )
-        # parser.add_argument(
-        #     f'--{constants.PUBSUBLITE_TO_GCS_WRITE_MODE}',
-        #     dest=constants.PUBSUBLITE_TO_GCS_WRITE_MODE,
-        #     required=False,
-        #     default=constants.OUTPUT_MODE_APPEND,
-        #     help=(
-        #         'Output write mode '
-        #         '(one of: append,overwrite,ignore,errorifexists) '
-        #         '(Defaults to append)'
-        #     ),
-        #     choices=[
-        #         constants.OUTPUT_MODE_OVERWRITE,
-        #         constants.OUTPUT_MODE_APPEND,
-        #         constants.OUTPUT_MODE_IGNORE,
-        #         constants.OUTPUT_MODE_ERRORIFEXISTS
-        #     ]
-        # )
-        # parser.add_argument(
-        #     f'--{constants.PUBSUBLITE_TO_GCS_TOTAL_RECEIVERS}',
-        #     dest=constants.PUBSUBLITE_TO_GCS_TOTAL_RECEIVERS,
-        #     required=False,
-        #     default=5,
-        #     help='PUBSUBLITE_TO_GCS_TOTAL_RECEIVERS'
-        # )
+        parser.add_argument(
+            f'--{constants.PUBSUBLITE_TO_GCS_WRITE_MODE}',
+            dest=constants.PUBSUBLITE_TO_GCS_WRITE_MODE,
+            required=False,
+            default=constants.OUTPUT_MODE_APPEND,
+            help=(
+                'Output write mode '
+                '(one of: append,overwrite,ignore,errorifexists) '
+                '(Defaults to append)'
+            ),
+            choices=[
+                constants.OUTPUT_MODE_OVERWRITE,
+                constants.OUTPUT_MODE_APPEND,
+                constants.OUTPUT_MODE_IGNORE,
+                constants.OUTPUT_MODE_ERRORIFEXISTS
+            ]
+        )
         parser.add_argument(
             f'--{constants.PUBSUBLITE_TO_GCS_OUTPUT_LOCATION}',
             dest=constants.PUBSUBLITE_TO_GCS_OUTPUT_LOCATION,
             required=True,
             help='GCS output Bucket URL'
         )
-        # parser.add_argument(
-        #     f'--{constants.PUBSUBLITE_TO_GCS_BATCH_SIZE}',
-        #     dest=constants.PUBSUBLITE_TO_GCS_BATCH_SIZE,
-        #     required=True,
-        #     help='Number of records to be written per message to GCS'
-        # )
-        # parser.add_argument(
-        #     f'--{constants.PUBSUBLITE_CHECKPOINT_LOCATION}',
-        #     dest=constants.PUBSUBLITE_CHECKPOINT_LOCATION,
-        #     required=True,
-        #     help='Temporary folder for checkpoint location'
-        # )
+        parser.add_argument(
+            f'--{constants.PUBSUBLITE_CHECKPOINT_LOCATION}',
+            dest=constants.PUBSUBLITE_CHECKPOINT_LOCATION,
+            required=True,
+            help='Temporary folder for checkpoint location'
+        )
+        parser.add_argument(
+            f'--{constants.PUBSUBLITE_TO_GCS_OUTPUT_FORMAT}',
+            dest=constants.PUBSUBLITE_TO_GCS_OUTPUT_FORMAT,
+            required=False,
+            default=constants.FORMAT_CSV
+            help=(
+                'Output Format to GCS '
+                '(one of: json, csv, avro, parquet) '
+                '(Defaults to csv)'
+            ),
+            choices=[
+                constants.FORMAT_AVRO,
+                constants.FORMAT_CSV,
+                constants.FORMAT_JSON,
+                constants.FORMAT_PRQT
+            ]
+        )
 
         known_args: argparse.Namespace
         known_args, _ = parser.parse_known_args(args)
@@ -112,16 +90,12 @@ class PubsubliteToGCSTemplate(BaseTemplate):
         logger: Logger = self.get_logger(spark=spark)
 
         # Arguments
-        input_project_id: str = args[constants.PUBSUBLITE_TO_GCS_INPUT_PROJECT_ID]
         output_project_id: str = args[constants.PUBSUBLITE_TO_GCS_OUTPUT_PROJECT_ID]
         input_subscription: str = args[constants.PUBSUBLITE_TO_GCS_INPUT_SUBSCRIPTION]
-        # timeout_ms: int = args[constants.PUBSUBLITE_TO_GCS_INPUT_TIMEOUT_MS]
-        # streaming_duration: int = args[constants.PUBSUBLITE_TO_GCS_STREAMING_DURATION_SECONDS]
-        # total_receivers: int = args[constants.PUBSUBLITE_TO_GCS_TOTAL_RECEIVERS]
         output_location: str = args[constants.PUBSUBLITE_TO_GCS_OUTPUT_LOCATION]
-        # batch_size: int = args[constants.PUBSUBLITE_TO_GCS_BATCH_SIZE]
-        # output_mode: str = args[constants.PUBSUBLITE_TO_GCS_WRITE_MODE]
-        # pubsublite_checkpoint_location: str = args[constants.PUBSUBLITE_CHECKPOINT_LOCATION]
+        output_mode: str = args[constants.PUBSUBLITE_TO_GCS_WRITE_MODE]
+        pubsublite_checkpoint_location: str = args[constants.PUBSUBLITE_CHECKPOINT_LOCATION]
+        output_format: str = args[constants.PUBSUBLITE_TO_GCS_OUTPUT_FORMAT]
 
         logger.info(
             "Starting Pubsublite to GCS spark job with parameters:\n"
@@ -131,39 +105,25 @@ class PubsubliteToGCSTemplate(BaseTemplate):
         spark = (
             SparkSession
                 .builder
-                .appName("read-app")
+                .appName("Pubsublite To GCS Dataproc Job")
                 .master("yarn")
                 .getOrCreate())
 
         # Read
         input_data=(spark.readStream \
             .format(constants.FORMAT_PUBSUBLITE) \
-            .option(f"{constants.FORMAT_PUBSUBLITE}.subscription",f"projects/{input_project_id}/locations/{os.getenv('REGION')}/subscriptions/{input_subscription}",) \
+            .option(f"{constants.FORMAT_PUBSUBLITE}.subscription", input_subscription,) \
             .load())
-
-        input_data.withColumn("data", input_data.data.cast(StringType()))
-        print("test test test")
-        print(input_data)
-        #logger.info(input_data)
 
         # Write
         query = (input_data.writeStream \
-            .format("csv") \
-            .option("temporaryGcsBucket", "gs://saumyasinha/pubsublite/tempBucket") \
-            .option("checkpointLocation", "gs://saumyasinha/pubsublite/checkpointLocation") \
+            .format(output_format) \
+            .option("checkpointLocation", pubsublite_checkpoint_location) \
             .option("path", output_location) \
+            .outputMode(output_mode) \
             .trigger(processingTime="1 second") \
             .start())
-        # query.awaitTermination(120)
-        query.stop()
-        # query = (input_data.writeStream \
-        #     .format(constants.FORMAT_BIGQUERY) \
-        #     .option("temporaryGcsBucket", bq_temp_bucket) \
-        #     .option("checkpointLocation", pubsublite_checkpoint_location) \
-        #     .option("table", f"{output_project_id}.{output_dataset}.{output_table}") \
-        #     .trigger(processingTime="1 second") \
-        #     .start())
 
         # Wait 120 seconds (must be >= 60 seconds) to start receiving messages.
-        # query.awaitTermination(120)
-        # query.stop()
+        query.awaitTermination(120)
+        query.stop()
