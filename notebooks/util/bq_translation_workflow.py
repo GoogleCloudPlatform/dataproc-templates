2ba213ae571d987d6d0165efa4c4edc9dfdb32cd
shubhamgoogle <shubu@google.com>
2023-05-02 13:33:58 +0000

    added bq translation api workflow

--- notebooks/util/bq_translation_workflow.py
+++ notebooks/util/bq_translation_workflow.py
@@ -1,15 +1,20 @@
 def create_migration_workflow(
-    gcs_input_path: str, gcs_output_path: str, project_id: str, bq_dataset: str, default_database: str, source_dilect: str, bq_region: str) -> None:
+    gcs_input_path: str, gcs_output_path: str, project_id: str, 
+    bq_dataset: str, default_database: str, source_dilect: str, bq_region: str,
+    obj_name_mapping: str = None) -> None:
+    """
+    obj_name_mapping:The mapping of objects to their desired output names in list form.
+    """
     from google.cloud import bigquery_migration_v2
     """Creates a migration workflow of a Batch SQL Translation and prints the response."""
     parent = f"projects/{project_id}/locations/{bq_region}"
     # Construct a BigQuery Migration client object.
     client = bigquery_migration_v2.MigrationServiceClient()
-    
     source_dialect = bigquery_migration_v2.Dialect()
     # Set the source dialect to Hive SQL.
     if source_dilect == "hive":
         source_dialect.hiveql_dialect = bigquery_migration_v2.HiveQLDialect()
+        migration_type="Translation_HiveQL2BQ"
     # Set the target dialect to BigQuery dialect.
     target_dialect = bigquery_migration_v2.Dialect()
     target_dialect.bigquery_dialect = bigquery_migration_v2.BigQueryDialect()
@@ -24,11 +29,11 @@ def create_migration_workflow(
                       bq_dataset
                     }
                     },
+        name_mapping_list=obj_name_mapping
        )
-    print(translation_config)
     # Prepare the task.
     migration_task = bigquery_migration_v2.MigrationTask(
-        type_=f"Translation_{source_dilect}QL2BQ", translation_config_details=translation_config)
+        type_=migration_type, translation_config_details=translation_config)
     # Prepare the workflow.
     workflow = bigquery_migration_v2.MigrationWorkflow(
         display_name=f"workflow-python-{source_dilect}2bq")
@@ -42,5 +47,18 @@ def create_migration_workflow(
     print(response.display_name)
     print("Current state:")
     print(response.State(response.state))
+    return response.name, response.state
        
 
+def get_migration_workflow_status(name):
+    # Create a client
+    from google.cloud import bigquery_migration_v2
+    client = bigquery_migration_v2.MigrationServiceClient()
+    # Initialize request argument(s)
+    request = bigquery_migration_v2.GetMigrationWorkflowRequest(
+        name=name,
+    )
+    # Make the request
+    response = client.get_migration_workflow(request=request)
+    # Return the response
+    return response
