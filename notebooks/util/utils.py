4148ac4764f8ea68c9ed7297866a07d0dc5ee270
shubhamgoogle <shubu@google.com>
2023-05-07 16:25:04 +0000

    Made changes to Readme File. Removed unwanted files

--- notebooks/util/utils.py
+++ notebooks/util/utils.py
@@ -3,7 +3,22 @@ def create_migration_workflow(
     bq_dataset: str, default_database: str, source_dilect: str, bq_region: str,
     obj_name_mapping: str = None) -> None:
     """
-    obj_name_mapping:The mapping of objects to their desired output names in list form.
+    This function uses BQ translation API to convert DDLs/SQLs from different sources to BQ
+
+    Args:
+        gcs_input_path (string): GCS location where source DDLs are available
+        gcs_output_path (string): Output GCS location
+        project_id (string): Project ID
+        bq_dataset (string): BQ Dataset ID to be added in the final DDL
+        default_database (string): Project ID to be added in the final DDL
+        source_dilect (string): Can be hive|redshift|netezza|teradata|synapse|mysql|oracle|postgresql|presto|sparksql|SQLserver|vertica
+        bq_region: Region of BQ Dataset
+        obj_name_mapping: The mapping of objects to their desired output names in list form.
+
+    Returns:
+        name (string): Full name of the Migration Job
+        state (string): Job Run State
+
     """
     from google.cloud import bigquery_migration_v2
     """Creates a migration workflow of a Batch SQL Translation and prints the response."""
@@ -51,6 +66,15 @@ def create_migration_workflow(
        
 
 def get_migration_workflow_status(name):
+    """
+    This function returns current running state of BQ translation API job
+
+    Args:
+        name (string): Full name of the Migration Job
+    Returns:
+        state (string): Job Run State
+
+    """
     # Create a client
     from google.cloud import bigquery_migration_v2
     client = bigquery_migration_v2.MigrationServiceClient()
@@ -62,33 +86,3 @@ def get_migration_workflow_status(name):
     response = client.get_migration_workflow(request=request)
     # Return the response
     return response
-
-def get_gcs_file_as_string(bucket,path):
-    import io
-    import os
-    from google.cloud import storage
-    # Create a storage client object.
-    client = storage.Client()
-    # Get the bucket object for the bucket that contains the text file.
-    bucket = client.get_bucket(bucket)
-    # Get the blob object for the text file.
-    blob = bucket.blob(path)
-    # Read the text file into a string.
-    text = blob.download_as_string().decode("utf-8")
-    # Return the text.
-    return text
-
-def run_bq_query(query):
-    # Import the necessary modules.
-    import io
-    import os
-    from google.cloud import bigquery
-    # Create a BigQuery client object.
-    client = bigquery.Client()
-    # Run the SQL query.
-    job = client.query(query)
-    # Wait for the job to finish.
-    job.result()
-    # Print the results of the query.
-    results = job.to_dataframe()
-    return results
