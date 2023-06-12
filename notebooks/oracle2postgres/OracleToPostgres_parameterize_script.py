b1c9f8ae24ae94ecef0694eeb53b7ff711daa8d0
vsinghal202 <vaibsinghal@google.com>
2023-06-12 06:57:05 +0000

    fix: Implement de-duplication

--- notebooks/oracle2postgres/OracleToPostgres_parameterize_script.py
+++ notebooks/oracle2postgres/OracleToPostgres_parameterize_script.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from os import environ
+from parameterize_script.util import get_common_args
 from typing import Dict, Sequence, Optional, Any
 import argparse
 import json
@@ -34,15 +34,6 @@ class OracleToPostgresScript(BaseParameterizeScript):
     @staticmethod
     def parse_args(args: Optional[Sequence[str]] = None) -> Dict[str, Any]:
         parser = argparse.ArgumentParser()
-
-        parser.add_argument(
-            f"--{constants.OUTPUT_NOTEBOOK_ARG}",
-            dest=constants.OUTPUT_NOTEBOOK_ARG,
-            required=False,
-            default=None,
-            help="Path to save executed notebook (Default: None). "
-            "If not provided, no notebook is saved",
-        )
 
         parser.add_argument(
             f"--{constants.ORACLE_HOST_ARG}",
@@ -147,32 +138,12 @@ class OracleToPostgresScript(BaseParameterizeScript):
             default=1000,
             help="JDBC output batch size (Default: 1000).",
         )
-
+        
+        parser = get_common_args(parser)
         known_args: argparse.Namespace
         known_args, _ = parser.parse_known_args()
 
         return vars(known_args)
-
-    def get_env_var(self, parameters) -> Dict[str, Any]:
-        """
-        Get the environment variables.
-        """
-        parameters[constants.PROJECT] = environ[constants.GCP_PROJECT]
-        parameters[constants.REGION] = environ[constants.REGION]
-        parameters[constants.GCS_STAGING_LOCATION] = environ[
-            constants.GCS_STAGING_LOCATION
-        ]
-        parameters[constants.SUBNET] = (
-            environ[constants.SUBNET] if constants.SUBNET in environ else ""
-        )
-        parameters[constants.SERVICE_ACCOUNT] = (
-            environ[constants.SERVICE_ACCOUNT]
-            if constants.SERVICE_ACCOUNT in environ
-            else ""
-        )
-        parameters[constants.IS_PARAMETERIZED] = True
-
-        return parameters
 
     def run(self, args: Dict[str, Any]) -> None:
         """
@@ -188,13 +159,14 @@ class OracleToPostgresScript(BaseParameterizeScript):
             args[constants.ORACLETABLE_LIST] = []
 
         # Exclude arguments that are not needed to be passed to the notebook
-        ignore_keys = {constants.OUTPUT_NOTEBOOK_ARG}
+        ignore_keys = {constants.LOG_LEVEL_ARG, constants.OUTPUT_NOTEBOOK_ARG}
         nb_parameters = {
             key: val for key, val in args.items() if key not in ignore_keys
         }
 
         # Get environment variables
-        nb_parameters = self.get_env_var(nb_parameters)
+        env_vars = OracleToPostgresScript.get_env_vars()
+        nb_parameters.update(env_vars)
 
         # Run the notebook
         output_path = args[constants.OUTPUT_NOTEBOOK_ARG]
