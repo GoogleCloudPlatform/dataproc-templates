220cd0e114e27e701d09afd7122646af463a1901
chakreshpatel <chakreshpatel@google.com>
2023-12-18 07:09:54 +0530

    feat: Built java KafkaToBQDstream template for Dataproc

--- java/src/main/java/com/google/cloud/dataproc/templates/util/TemplateConstants.java
+++ java/src/main/java/com/google/cloud/dataproc/templates/util/TemplateConstants.java
@@ -498,4 +498,9 @@ public interface TemplateConstants {
   String TEXT_BIGQUERY_DELIMITER_OPTION = "delimiter";
   String TEXT_BIGQUERY_INFERSCHEMA_OPTION = "inferSchema";
   String TEXT_BIGQUERY_HEADER_OPTION = "header";
+
+  /** Kafka To BQ - Direct stream properties */
+  String KAFKA_BQ_CONSUMER_GROUP_ID = "kafka.bq.consumer.group.id";
+
+  String KAFKA_BQ_BATCH_INTERVAL = "kafka.bq.batch.interval";
 }
