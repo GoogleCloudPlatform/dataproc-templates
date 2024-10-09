a6eb36471793de77709154a6e01fa98488939e16
vanshaj-bhatia <105878445+vanshaj-bhatia@users.noreply.github.com>
2024-10-09 21:04:08 +0530

    Revert "ci: ui integration test for release (#962)" (#973)
    This reverts commit 28c155fbf1e3322de9e9fbe376b1b5078c565042.

--- python/setup.py
+++ python/setup.py
@@ -25,7 +25,7 @@ from typing import Optional
 import shutil
 import io
 import os
-import logging
+from distutils import log
 from setuptools import setup, find_packages
 from setuptools.command.bdist_egg import bdist_egg
 
@@ -66,7 +66,7 @@ class BdistEggCustomEggName(bdist_egg):
         built_egg_path: str = self.get_outputs()[0]
 
         if self.output is not None:
-            logging.info(
+            log.info(
                 'Will rename output .egg file from %s to %s',
                 built_egg_path,
                 self.output
