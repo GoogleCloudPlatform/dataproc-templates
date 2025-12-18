7ac749ba79442d4fd33871e001ead22f015394b5
Sundar Mudupalli <106282841+sundar-mudupalli-work@users.noreply.github.com>
2025-12-18 01:49:08 +0000

    insertion_time function

---
+++ gemini-codegen/java/src/main/java/com/google/cloud/dataproc/templates/util/DataframeUtils.java
@@ -0,0 +1,18 @@
+package com.google.cloud.dataproc.templates.util;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.functions;
+
+public class DataframeUtils {
+
+    /**
+     * Adds an "insertion_time" column to the given Spark DataFrame with the current timestamp.
+     *
+     * @param dataframe The input Spark DataFrame.
+     * @return A new Spark DataFrame with the "insertion_time" column added.
+     */
+    public static Dataset<Row> addInsertionTimeColumn(Dataset<Row> dataframe) {
+        return dataframe.withColumn("insertion_time", functions.current_timestamp());
+    }
+}
